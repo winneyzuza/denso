e2ae591bdb77f768de306db68eeb9170a6e5be0f
DTBTBAK1 <DTBTBAK1@DTBTBAK1-TEAM>
2016-10-21 03:51:53 +0700

    New : [RELEASE2] 1.7 Add more Dealer for other region [Backend] - SQL

---
+++ DBPool/db_script_release2.sql
@@ -0,0 +1,14 @@
+﻿--DB script : Release 2
+------------------------------------------------------------------------------
+
+ALTER TABLE region ADD COLUMN 'region_name_th' TEXT NOT NULL AFTER 'region_name';
+
+UPDATE region SET  region_name_th = 'กรุงเทพและปริมณฑล' WHERE region_code ='BKK';
+UPDATE region SET  region_name_th = 'ตะวันออก' WHERE region_code ='E';
+UPDATE region SET  region_name_th = 'เหนือ' WHERE region_code ='N';
+UPDATE region SET  region_name_th = 'ตะวันออกเฉียงเหนือ' WHERE region_code ='N-E';
+UPDATE region SET  region_name_th = 'ใต้' WHERE region_code ='S';
+UPDATE region SET  region_name_th = 'ตะวันตก' WHERE region_code ='W';
+COMMIT;
+
+------------------------------------------------------------------------------
